7bae551fa6d3370b990604d9e8cb58f7a4759dae
Daniel Kobiałka <daniel.kobialka.stud@pw.edu.pl>
2022-11-29 19:13:30 +0100

    Code refactored

--- 1_3/server.py
+++ 1_3/server.py
@@ -4,14 +4,21 @@ import io
 
 HOSTNAME, PORT, MAXDATA = "127.0.0.1", 8081, 1024
 
+NETWORK_BYTE_ORDER = "!"
+PADDING = "4x"
+STRING = "10s"
+LONG = "l"
+SHORT = "h"
+FORMAT = NETWORK_BYTE_ORDER + LONG + PADDING + SHORT + STRING + PADDING
+
 
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.bind((HOSTNAME, PORT))
         while True:
             data = s.recvfrom(MAXDATA)
-            result = struct.unpack("!l4xh10s4x", data[0])
-            print(result)
+            long, short, string = struct.unpack(FORMAT, data[0])
+            print(f"long: {long}, short: {short}, string: {string}")
 
 
 if __name__ == "__main__":
