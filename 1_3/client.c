e3d4f61d44e4b7ff249a909fb716263ba8f8179a
Daniel Kobiałka <daniel.kobialka.stud@pw.edu.pl>
2022-11-29 18:59:34 +0100

    Added sending struct from C to python

---
+++ 1_3/client.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#define HOSTNAME "127.0.0.1"
+
+struct test_struct {
+    long int a;
+    short int b;
+    char c[10];
+};
+
+void prepare_struct(struct test_struct* ts) {
+    ts->a = htonl(ts->a);
+    ts->b = htons(ts->b);
+}
+
+int main() {
+    int sock;
+    struct sockaddr_in cliaddr;
+    struct hostent *hp;
+
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock == -1) {
+        perror("opening datagram socket failed");
+        exit(1);
+    }
+    hp = gethostbyname(HOSTNAME);
+    if (hp == (struct hostent *) 0) {
+        fprintf(stderr, "%s: unknown host\n", HOSTNAME);
+        exit(2);
+    }
+    memcpy((char *) &cliaddr.sin_addr, (char *) hp->h_addr,
+           hp->h_length);
+
+    cliaddr.sin_family = AF_INET;
+    cliaddr.sin_port = htons(8081);
+
+    struct test_struct a;
+    a.a = 2137;
+    a.b = 420;
+    strcpy(a.c, "John Paul");
+    prepare_struct(&a);
+
+    if (sendto(sock, &a, sizeof (struct test_struct), MSG_CONFIRM, (const struct sockaddr *) &cliaddr,
+        sizeof(cliaddr)) == -1) {
+        perror("sending datagram message failed");
+    }
+
+    close(sock);
+    return 0;
+}
+
---
+++ 1_3/server.py
@@ -0,0 +1,18 @@
+import socket
+import struct
+import io
+
+HOSTNAME, PORT, MAXDATA = "127.0.0.1", 8081, 1024
+
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.bind((HOSTNAME, PORT))
+        while True:
+            data = s.recvfrom(MAXDATA)
+            result = struct.unpack("!l4xh9s5x", data[0])
+            print(result)
+
+
+if __name__ == "__main__":
+    main()
