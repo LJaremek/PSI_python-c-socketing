8ad2b1163d93ca47eada63508b86baa9238a6877
Daniel Kobiałka <daniel.kobialka.stud@pw.edu.pl>
2022-12-02 13:11:36 +0100

    1_1/python_Impl Made port and hostname cmdline args

--- 1_1/python_implementation/server/server.py
+++ 1_1/python_implementation/server/server.py
@@ -1,14 +1,19 @@
 import socket
+import sys
 
-HOSTNAME, PORT, MAXDATA = "0.0.0.0", 8081, 1024 # parametryzacja portu
+HOSTNAME, MAXDATA = "0.0.0.0", 1024
 
 
-def main():
+def main(argc, argv):
+    if (argc < 2):
+        print(f"Usage: {argv[0]} <PORT>")
+        exit(1)
+    port = int(argv[1])
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        s.bind((HOSTNAME, PORT))
+        s.bind((HOSTNAME, port))
         while True:
             print(f"Python server received : {s.recvfrom(MAXDATA)[0].decode('utf-8')}")
 
 
 if __name__ == "__main__":
-    main()
+    main(len(sys.argv), sys.argv)
