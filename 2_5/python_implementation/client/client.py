caba4989d9c3d70d8df9331f660a9dd41a518500
Lukasz Jaremek <Jaremek.Lukasz@gmail.com>
2022-12-10 13:23:35 +0100

    task 2.5 done

---
+++ 2_5/python_implementation/client/client.py
@@ -0,0 +1,26 @@
+import socket
+import sys
+
+MAXDATA, DATA = 1024, "hello from python TCP client"
+
+
+def main(argc: int, argv: list[str]) -> None:
+    # get initial arguments
+    if argc < 3:
+        print(f"Usage: {argv[0]} <HOSTNAME> <PORT>")
+        exit(1)
+    hostname = argv[1]
+    port = int(argv[2])
+
+    # create stream socket
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        # connect with server
+        s.connect((hostname, port))
+
+        # send data to server
+        s.sendall(DATA.encode("utf-8"))
+        print(f"Client sent : {DATA}")
+
+
+if __name__ == "__main__":
+    main(len(sys.argv), sys.argv)
---
+++ 2_5/python_implementation/server/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -it --rm --network-alias z14_network --network z14_network --name z14_server_25_py --ip 172.21.14.4 z14_server_25_py "$@"
