52ee6600f0b016aba2b4ccc5564170c41c3a763f
Radosław Kostrzewski <01158754@pw.edu.pl>
2022-12-15 13:47:53 +0100

    Add fork to tcp server in C

---
+++ 2_5/C_implementation/client/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker image rm z14_client_25_c
+docker build -t z14_client_25_c .
---
+++ 2_5/C_implementation/client/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -it --rm --network-alias z14_network --network z14_network --name z14_client_25_c z14_client_25_c "$@"
