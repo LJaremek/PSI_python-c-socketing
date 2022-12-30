e67cc7708b8e3b5ffa3b7afa6baad2b4cdc6d90a
Lukasz Jaremek <Jaremek.Lukasz@gmail.com>
2022-12-30 18:33:47 +0100

    create _create_server_socket, _create_client_sockets and Socket class (it is normal socket.socket with variable name)

--- 3/node.py
+++ 3/node.py
@@ -1,25 +1,65 @@
+
+from socket import AF_INET, SOCK_STREAM
 from socket import socket
 import json
 import os
 
 
+class Socket(socket):
+    def __init__(self, node_name: str, *args, **kwargs) -> None:
+        super(Socket, self).__init__(*args, **kwargs)
+        self._name = node_name
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+
 class Node:
-    def __init__(self, config_file_path: str = "config.json") -> None:
+    def __init__(
+            self,
+            node_name: str,
+            node_addr: str,
+            node_port: str,
+            config_file_path: str = "config.json"
+            ) -> None:
+
         self._config_data = self._read_config_file(config_file_path)
         self._nodes: list[dict] = self._config_data["nodes"]
+
+        self._create_server_socket(node_name, node_addr, node_port)
+
+        self._client_sockets: list[Socket] = []
+        self._create_client_sockets(self._nodes)
 
     def _read_config_file(self, config_file_path: str) -> list[dict]:
         with open(config_file_path, "r", -1, "utf-8") as f:
             return json.load(f)
 
-    def _create_socket(
+    def _create_server_socket(
             self,
             node_name: str,
             node_addr: str,
             node_port: str
             ) -> socket:
-        # TODO: gniazda xd
-        ...
+
+        self._server_socket = Socket(node_name, AF_INET, SOCK_STREAM)
+        self._server_socket.bind((node_addr, node_port))
+        self._server_socket.listen(5)
+        # TODO: puścić server na Thread
+
+    def _create_client_sockets(self, client_nodes_data: list[dict]) -> None:
+        for node_data in client_nodes_data:
+            node_name = node_data["node_name"]
+            # node_addr = node_data["node_addr"]
+            # node_port = node_data["node_port"]
+
+            if node_name == self._server_socket.name:
+                continue
+
+            client_socket = Socket(node_name, AF_INET, SOCK_STREAM)
+            self._client_sockets.append(client_socket)
+            # TODO: wywołanie connect z node_addr i node_port
 
     def available_files(self) -> list[str]:
         # TODO: pobieranie dostępnych plików z innych węzłów (UDP)
